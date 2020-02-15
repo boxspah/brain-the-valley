876d763e3105e928a7c0289b9f9c1988b7145b73
Aryan Gajelli <argajelli@gmail.com>
2020-02-15 09:21:33 -0500

    Keyboard stuff
    You need to pip install keyboard

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import eeg
+#mport eeg
+import keyboard as kb
+
+kb.add_hotkey('a', lambda: keyboard.write('Geek'))
+kb.add_hotkey('ctrl + shift + a', print, args=('you entered', 'hotkey'))
+
+kb.wait('esc')
