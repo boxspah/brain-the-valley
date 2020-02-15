2e603536c2d48481a84b29307548e8f1a33b48f1
Aryan Gajelli <argajelli@gmail.com>
2020-02-15 14:36:02 -0500

    Added a cortex class that can handle all the requests and issues with the headset

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import connect
+from cortex import Cortex
+
+connection_url = "wss://localhost:6868"
+user = {
+    "client_id": "NZjtUNCOiaPP7gMkNSucchM7jATzNY386Aq2hMI7",
+    "client_secret": "526x2Afu46XG6eMMfkJ7Him3QKszzys9Bs4ABKcAVhLa6xfIfSbTs1ZmziBqYcOHWvup3N9XqBo9GbhMAqh2sWSGKQgj5k30PQUwocFaq1haP4eb3oUKMlUp70ZmoOG9",
+    "license": "",
+    "debit": 500
+
+}
+headset = Cortex(connection_url,user)
+headset.grant_access_and_session_info()
