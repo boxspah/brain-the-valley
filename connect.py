77e240e0c65d6033528310731d7c1e1ca4e66eea
Aryan Gajelli <argajelli@gmail.com>
2020-02-15 13:41:08 -0500

    Connects to the headset properly

--- connect.py
+++ connect.py
@@ -3,6 +3,7 @@ from websocket import create_connection
 import ssl
 import time
 import requests
+import warnings
 
 ws = create_connection("wss://localhost:6868", sslopt={"cert_reqs": ssl.CERT_NONE})
 
@@ -15,32 +16,56 @@ def recieve_packet():
     return json.loads(ws.recv())
 
 
-def send_request(json_block, recieve_print_result=False):
+def send_request(json_block, recieve_result=False):
     """
     Sends a json packet to the headset and prints the recieved result
     @param json_block The json packet to send
-    @param recieve_print_result wether the result should be recieved and printed
+    @param recieve_result wether the result should be recieved
     """
     ws.send(json.dumps(json_block))
-    if recieve_print_result:
-        print(recieve_packet())
+    if recieve_result:
+        return recieve_packet()
 
 
-send_request(
-    {
-        "id": 1,
-        "jsonrpc": "2.0",
-        "method": "queryHeadsets"
-    }
-)
+def connect_to_headset():
+    """
+    Connects to headset
+    @returns the connection result if successful, error 400 if failed
+    """
 
-connection = recieve_packet()
+    connect_tries = 0
+    while True:
+        try:
+            send_request(
+                {
+                    "id": 1,
+                    "jsonrpc": "2.0",
+                    "method": "queryHeadsets"
+                }
+            )
+            connect = recieve_packet()
+            connect_tries += 1
+            time.sleep(0.5)
+            if connect_tries > 10:
+                raise TimeoutError
+            if not len(connect["result"]):
+                raise ConnectionError
+            return connect
+        except ConnectionError:
+            print("Headset not found")
+            continue
+        except TimeoutError:
+            print("Too many failed Attempts")
+            return 400
+        except Exception(e):
+            print(e)
+            return e
 
-if not len(connection["result"]):
-    print("Headset not found")
+
+connection = connect_to_headset()
+
+if connection == Exception or connection == 400:  # checks whether to continue with the program
     exit(0)
-
-headset_id = connection["result"][0]["id"]
 
 print(connection, end='\n\n')
 print(headset_id)
@@ -57,5 +82,5 @@ send_request(
     }
 )
 
-connection = json.loads(ws.recv())
+connection = recieve_packet()
 print(connection)
