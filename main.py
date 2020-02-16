14b77b0a8d976e6502cf3ba6e173eb918a18ab4a
Aryan Gajelli <argajelli@gmail.com>
2020-02-16 01:09:00 -0500

    Updated files: GUI will run first, gyro disabled

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from pynput.mouse import Button, Controller
 import json
 from helper import *
 import time
-
+from win32api import GetSystemMetrics
+screen_w, screen_h = GetSystemMetrics(0), GetSystemMetrics(1)
 connection_url = "wss://localhost:6868"
 user = {
     "client_id": "NZjtUNCOiaPP7gMkNSucchM7jATzNY386Aq2hMI7",
@@ -14,38 +15,33 @@ user = {
     "license": "",
     "debit": 500
 }
+
 headset = Cortex(connection_url, user)
-headset.grant_access_and_session_info()
+
+
+main = Control()
+main.setup(headset)
+main.run_mainloop()
 headset.setup_profile("Asad")
 headset.subRequest(["mot"])
 
 ms = Controller()
 
 zero_pos = [0, 0, 0]
-screen_w = 1920
-screen_h = 1080
 rot = [0, 0, 0]
-def on_press(key):
-    try:
 
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+
+def on_press(key):
+    return None
 
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
-
     if key == keyboard.Key.space:
         global zero_pos
-        zero_pos= rot
+        zero_pos = rot
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-
 
 
 # ...or, in a non-blocking fashion:
@@ -53,45 +49,31 @@ listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
-prev = [screen_h/2, screen_w/2,0]
-ms_list = []
+prev = [screen_h / 2, screen_w / 2, 0]
 ms.position = (0, 0)
-while True:
 
+while True:
     signal = headset.recieve_signal()
     quaternion = signal["mot"][2:6]
     r = R.from_quat(quaternion)
-    rot = list(map(lambda x: x if x>0 else x+360, r.as_euler('xzy', degrees=True)))
-    print("rot", rot,end=' ')
+    rot = list(map(lambda x: x if x > 0 else x + 360, r.as_euler('xzy', degrees=True)))
+    print("rot", rot, end=' ')
     conv = [scale(
-                clamp(rot[0],zero_pos[0],zero_pos[0]-14),
-                zero_pos[0],
-                zero_pos[0]-14,
-                0,
-                screen_w
-            ),
-            scale(
-                clamp(rot[1],zero_pos[1] ,zero_pos[1]-7),
-                zero_pos[1],
-                zero_pos[1]-7,
-                screen_h,
-                0
-            ),
+        clamp(rot[0], zero_pos[0], zero_pos[0] - 14),
+        zero_pos[0],
+        zero_pos[0] - 14,
+        0,
+        screen_w
+    ),
+        scale(
+            clamp(rot[1], zero_pos[1], zero_pos[1] - 7),
+            zero_pos[1],
+            zero_pos[1] - 7,
+            screen_h,
+            0
+        ),
         0
-        ]
+    ]
     conv = list(map(lambda x: int(x), conv))
     print("conv", conv)
-    prev = ema_filter(conv, prev, [0.7,0.7,0.7])
-
-    ms_list.append(tuple(conv[0:2]))
-    if len(ms_list) > 6:
-        ms.position = (sum(x for x,y in ms_list)/6, sum(y for x, y in ms_list)/6 )
-        ms_list.pop(0)
-
-
-
-main = Control()
-main.setup(headset)
-
-
-# main.run_mainloop()
+    prev = ema_filter(conv, prev, [0.7, 0.7, 0.7])
